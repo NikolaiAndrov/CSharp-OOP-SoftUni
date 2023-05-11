819c2c6c130fe902b9c142523dd39b2046069f3a
NikolaiAndrov <32267207+NikolaiAndrov@users.noreply.github.com>
2023-05-11 23:03:58 +0300

    Add files via upload

---
+++ EncapsulationLab/PersonsInfo/StartUp.cs
@@ -0,0 +1,41 @@
+﻿namespace PersonsInfo
+{
+    public class StartUp
+    {
+        public static void Main()
+        {
+            var n = int.Parse(Console.ReadLine());
+            var people = new List<Person>();
+
+            for (int i = 0; i < n; i++)
+            {
+                try
+                {
+                    var personInfo = Console.ReadLine()
+                    .Split(" ", StringSplitOptions.RemoveEmptyEntries);
+                    var firstName = personInfo[0];
+                    var lastName = personInfo[1];
+                    var age = int.Parse(personInfo[2]);
+                    var salary = decimal.Parse(personInfo[3]);
+                    var person = new Person(firstName, lastName, age, salary);
+                    people.Add(person);
+                }
+                catch (Exception e)
+                {
+
+                    Console.WriteLine(e.Message);
+                }
+                
+            }
+
+            Team team = new Team("SoftUni");
+
+            foreach (var person in people)
+            {
+                team.AddPlayer(person);
+            }
+
+            Console.WriteLine(team);
+        }
+    }
+}
---
+++ EncapsulationLab/PersonsInfo/Team.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PersonsInfo
+{
+    public class Team
+    {
+        private string name;
+        private List<Person> firstTeam;
+        private List<Person> reserveTeam;
+
+        public Team(string name)
+        {
+            this.Name = name;
+            this.firstTeam = new List<Person>();
+            this.reserveTeam = new List<Person>();
+        }
+
+        public string Name
+        {
+            get { return name; }
+            private set { name = value; }
+        }
+        public IReadOnlyCollection<Person> FirstTeam
+        { 
+            get { return this.firstTeam.AsReadOnly(); }
+        }
+
+        public IReadOnlyCollection<Person> ReserveTeam
+        {
+           get { return reserveTeam.AsReadOnly(); }
+        }
+
+        public void AddPlayer(Person person)
+        {
+            if (person.Age < 40)
+            {
+                this.firstTeam.Add(person);
+            }
+            else
+            {
+                this.reserveTeam.Add(person);
+            }
+        }
+
+        public override string ToString()
+        {
+            StringBuilder sb = new StringBuilder();
+            sb.AppendLine($"First team has {FirstTeam.Count} players.");
+            sb.Append($"Reserve team has {ReserveTeam.Count} players.");
+            return sb.ToString();
+        }
+    } 
+}
