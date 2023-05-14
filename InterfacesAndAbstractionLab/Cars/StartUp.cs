a23990c9f09b41a9c139d960c4f8264e17c3d72f
NikolaiAndrov <32267207+NikolaiAndrov@users.noreply.github.com>
2023-05-14 21:23:24 +0300

    Add files via upload

---
+++ InterfacesAndAbstractionLab/Cars/StartUp.cs
@@ -0,0 +1,14 @@
+﻿namespace Cars
+{
+    public class StartUp
+    {
+        public static void Main()
+        {
+            ICar seat = new Seat("Leon", "Grey");
+            Console.WriteLine(seat.ToString());
+
+            ICar tesla = new Tesla("Model 3", "Red", 2);
+            Console.WriteLine(tesla.ToString());
+        }
+    }
+}
---
+++ InterfacesAndAbstractionLab/Shapes/Circle.cs
@@ -0,0 +1,35 @@
+﻿namespace Shapes
+{
+    public class Circle : IDrawable
+    {
+        private int radius;
+
+        public Circle(int radius)
+        {
+            this.radius = radius;
+        }
+        public void Draw()
+        {
+            double rin = this.radius - 0.4;
+            double rout = this.radius + 0.4;
+
+            for (double y = this.radius; y >= -this.radius; --y)
+            {
+                for (double x = -this.radius; x < rout; x += 0.5)
+                {
+                    double value = x * x + y * y;
+
+                    if (value >= rin * rin && value <= rout * rout)
+                    {
+                        Console.Write("*");
+                    }
+                    else
+                    {
+                        Console.Write(" ");
+                    }
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
